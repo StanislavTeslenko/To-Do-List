daaa2c25372ce506edd7b40fc14f311fc2102600
Stanislav Teslenko <stanislav.teslenko@gmail.com>
2019-06-30 14:42:53 +0300

    cleaned App Delegate

---
+++ To Do List/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  To Do List
+//
+//  Created by Stanislav Teslenko on 6/26/19.
+//  Copyright © 2019 Stanislav Teslenko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
